3402cd75886aab365187b24b2773e9b61c2b75ee
AnithaThilagar <anithathilagar@gmail.com>
2018-09-27 14:24:14 +0530

    Code commit

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.post('/generateToken', async (request, response) => {
 	let urlParts = url.parse(request.headers.referer, true);
 	//console.log(urlParts.query);
 	await getAccessToken(request.body).then((token) => {
-		console.log("Before redirect ", urlParts.query.redirect_uri+"&state=xyz&code=SplxlOBeZQQYbYS6WxSbIA");
-		response.redirect(urlParts.query.redirect_uri+"state=SplxlOBeZQQYbYS6WxSbIA&token_type=Bearer&access_token="+token.replace('Bearer ',''));
+		console.log("Before redirect ");
+		response.redirect(urlParts.query.redirect_uri+"&state=SplxlOBeZQQYbYS6WxSbIA&token_type=Bearer&access_token="+token.replace('Bearer ',''));
 		//response.redirect("https://pitangui.amazon.com/api/skill/link/M2J3G9PSOAGI37?vendorId=M2J3G9PSOAGI37#state=SplxlOBeZQQYbYS6WxSbIA&token_type=Bearer&access_token="+token.replace('Bearer ',''));
 	}).catch((error) => {
 		console.log("Error in accessToken ", error);
