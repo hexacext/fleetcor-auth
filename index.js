0078e17aee45f8eaf00375f4b60e2306e2019f50
AnithaThilagar <anithathilagar@gmail.com>
2018-09-28 09:20:14 +0530

    Code commit

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/generateToken', async (request, response) => {
 	let urlParts = url.parse(request.headers.referer, true);
 	console.log(urlParts.query);
 	await getAccessToken(request.body).then((token) => {
-		console.log("Before redirect ");
+		console.log("Before redirect ",urlParts.query.redirect_uri+"&response_type=code&state="+urlParts.query.state+"&code=SplxlOBeZQQYbYS6WxSbIA");
 		response.redirect(urlParts.query.redirect_uri+"&response_type=code&state="+urlParts.query.state+"&code=SplxlOBeZQQYbYS6WxSbIA");
 	}).catch((error) => {
 		console.log("Error in accessToken ", error);
