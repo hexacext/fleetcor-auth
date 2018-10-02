dedcc873689e5f8a91a3a16d7ef043e2258b9c5e
AnithaThilagar <anithathilagar@gmail.com>
2018-10-02 21:27:02 +0530

    Code commit

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/accessToken', async (request, response) => {
 			response.send(authData);
 		} else {
 			authData.token_type = "bearer";
-			authData.expires_in = 3600;
+			authData.expires_in = 360;
 			authData.scope = "profile";
 			//console.log("Send data ", authData);
 			response.send(authData);
